7899c73ae2fc2aa3139d00cf0502caa813e6e2c1
barkindagda <98694978+barkindagda@users.noreply.github.com>
2024-02-26 11:55:14 +0000

    Update test.py

--- TransGeo/test.py
+++ TransGeo/test.py
@@ -2,12 +2,17 @@ import numpy as np
 import pandas as pd
 import math
 import scipy.spatial
-K = 1 # Number of top features to consider
-DISTANCE_THRESHOLD = 0.0 
-ref_features_path='./'
-query_features_path = './'
-ref_filenames_path = './'
-query_filenames_path = './'
+import argparse
+
+# Setup argparse
+parser = argparse.ArgumentParser(description='GeoImage Matching Script')
+parser.add_argument('--ref_features_path', type=str, required=True, help='Path to the reference features .npy file')
+parser.add_argument('--query_features_path', type=str, required=True, help='Path to the query features .npy file')
+parser.add_argument('--ref_filenames_path', type=str, required=True, help='Path to the reference filenames .csv file')
+parser.add_argument('--query_filenames_path', type=str, required=True, help='Path to the query filenames .csv file')
+parser.add_argument('--k', type=int, default=1, help='Number of top features to consider')
+parser.add_argument('--distance_threshold', type=float, default=0.0, help='Distance threshold for matching')
+args = parser.parse_args()
 
 
 ref_features = np.load(ref_features_path)
@@ -32,42 +37,32 @@ def extract_gps_from_filename(filename):
     lat = float(parts[1])
     lon = float(parts[2].split('.')[0])  # Remove the file extension
     return lat, lon
+def find_top_k_features(query_feature, ref_features, k):
+    if len(query_feature.shape) != 1:
+        query_feature = query_feature.flatten()
+    similarities = [1 - scipy.spatial.distance.cosine(query_feature, ref_feature)
+                    for ref_feature in ref_features]
+    top_k_indices = np.argsort(similarities)[-k:]
+    return top_k_indices
 
-# GeoImageMatcher class
-class GeoImageMatcher:
-    def __init__(self, ref_features, ref_filenames_df, query_features, query_filenames_df):
-        self.reference_features = ref_features
-        self.query_features = query_features
-        self.reference_filenames_df = ref_filenames_df
-        self.query_filenames_df = query_filenames_df
+def calculate_top_k_accuracy(query_features, query_filenames_df, ref_features, ref_filenames_df, k, distance_threshold):
+    correct_in_top_k_count = 0
+    for i, query_feature in enumerate(query_features):
+        top_k_indices = find_top_k_features(query_feature, ref_features, k)
+        query_filename = query_filenames_df.iloc[i]['filename']
+        query_lat, query_lon = extract_gps_from_filename(query_filename)
 
-    def find_top_k_features(self, query_feature, k):
-        if len(query_feature.shape) != 1:
-            query_feature = query_feature.flatten()
-        similarities = [1 - scipy.spatial.distance.cosine(query_feature, ref_feature)
-                        for ref_feature in self.reference_features]
-        top_k_indices = np.argsort(similarities)[-k:]
-        return top_k_indices
+        for idx in top_k_indices:
+            ref_filename = ref_filenames_df.iloc[idx]['filename']
+            ref_lat, ref_lon = extract_gps_from_filename(ref_filename)
+            distance = haversine(query_lon, query_lat, ref_lon, ref_lat)
+            if distance <= distance_threshold:
+                correct_in_top_k_count += 1
+                break
 
-    def calculate_top_k_accuracy(self, k, distance_threshold):
-        correct_in_top_k_count = 0
-        for i, query_feature in enumerate(self.query_features):
-            top_k_indices = self.find_top_k_features(query_feature, k)
-            query_filename = self.query_filenames_df.iloc[i]['filename']
-            query_lat, query_lon = extract_gps_from_filename(query_filename)
-
-            for idx in top_k_indices:
-                ref_filename = self.reference_filenames_df.iloc[idx]['filename']
-                ref_lat, ref_lon = extract_gps_from_filename(ref_filename)
-                distance = haversine(query_lon, query_lat, ref_lon, ref_lat)
-                if distance <= distance_threshold:
-                    correct_in_top_k_count += 1
-                    break
-
-        accuracy = (correct_in_top_k_count / len(self.query_features)) * 100
-        return accuracy
+    accuracy = (correct_in_top_k_count / len(query_features)) * 100
+    return accuracy
 
 # Usage
-matcher = GeoImageMatcher(ref_features, ref_filenames_df, query_features, query_filenames_df)
-top_k_accuracy = matcher.calculate_top_k_accuracy(K, DISTANCE_THRESHOLD)
-print(f"Top-{K} Accuracy: {top_k_accuracy}%")
+top_k_accuracy = calculate_top_k_accuracy(query_features, query_filenames_df, ref_features, ref_filenames_df, args.K, args.distance_threshold)
+print(f"Top-{args.K} Accuracy: {top_k_accuracy}%")
